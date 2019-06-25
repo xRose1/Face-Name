8b3ff3ccb540fea6f5153bec3a22dc7689b91002
Sanjeev Bedasee <ibedasee@gmail.com>
2019-06-24 20:49:27 -0400

    first commit

---
+++ Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Sbedx4 on 06/24/2019.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+	var name: String
+	var image: String
+
+	init(name: String, image: String) {
+		self.name = name
+		self.image = image
+	}
+}
---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Sbedx4 on 06/24/2019.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var name: UILabel!
+}
